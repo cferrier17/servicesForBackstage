96d721796d7800f41772f1c9580dd8d6d2a2cce4
Ferrier <ferrier.corentin@wanadoo.fr>
2019-01-30 11:06:31 +0100

    Fin TD1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void toastMe(View view){
+        Toast myToast = Toast.makeText(this, "Bon toutou :)", Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+
+    public void countMe(View view){
+        TextView showCountTextView = (TextView) findViewById(R.id.textViewCount);
+
+        String countString = showCountTextView.getText().toString();
+
+        Integer count = Integer.parseInt(countString);
+        count ++;
+
+        showCountTextView.setText(count.toString());
+    }
+
+    public void randomMe(View view){
+        TextView showRandomTextView = (TextView) findViewById(R.id.textViewCount);
+
+        String randomString = showRandomTextView.getText().toString();
+
+        Integer rand = (int)(Math.random() * ((100) + 1));
+
+
+        showRandomTextView.setText(rand.toString());
+    }
+
+    public void bonToutou(View view){
+        Intent toutouIntent = new Intent(this, secondActivity.class);
+
+        startActivity(toutouIntent);
+    }
 }
-    
